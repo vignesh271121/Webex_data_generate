4b0fe22db6fa23932cb355ebb66442d190a9f45d
vignesh271121 <97229745+vignesh271121@users.noreply.github.com>
2022-01-17 11:40:12 +0530

    Update views.py

--- report_App/views.py
+++ report_App/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from . import report_download
+from django.contrib import messages
 
 
 def index(request):
@@ -13,7 +14,8 @@ def download_data(request):
         token_Get = str(request.POST.get("token"))
         date_Get = str(request.POST.get("date_val"))
         room_id_Get = str(request.POST.get("room_id"))
-        report_download.download(room_id_Get,date_Get,token_Get,)
-        return render(request, 'excel_report.html')
+        download_file = report_download.download(room_id_Get,date_Get,token_Get,)
+        #return render(request, 'excel_report.html')
+        return render(request, 'excel_report.html',{'alert_flag': "True",'file_name':download_file})
     else:
         return render(request, 'excel_report.html')
